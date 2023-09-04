04ac64a8d880b2c3fef5e029ef36b991b2cb3ebc
GeunHyeok Jeong <jkh990521@gmail.com>
2023-09-04 16:11:59 +0900

    a

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,18 +4,11 @@ import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-//
+import { Container } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
+
 const Navbar = ({ authenticate, setAuthenticate }) => {
-  const menuList = [
-    "여성",
-    "Divided",
-    "남성",
-    "신생아/유아",
-    "아동",
-    "H&M HOME",
-    "Sale",
-    "지속가능성",
-  ];
+  const menuList = ["게시판", "정보제공", "고객센터", "마이페이지"];
   let [width, setWidth] = useState(0);
   let navigate = useNavigate();
   const onCheckEnter = (event) => {
@@ -35,28 +28,33 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
           ))}
         </div>
       </div>
-      <div className="nav-header">
-        <div className="burger-menu hide">
-          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
-        </div>
-        {authenticate ? (
-          <div onClick={() => setAuthenticate(false)}>
-            <FontAwesomeIcon icon={faUser} />
-            <span style={{ cursor: "pointer" }}>로그아웃</span>
-          </div>
-        ) : (
-          <div onClick={() => navigate("/login")}>
-            <FontAwesomeIcon icon={faUser} />
-            <span style={{ cursor: "pointer" }}>로그인</span>
-          </div>
-        )}
+
+      <div className="burger-menu hide">
+        <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
       </div>
 
-      <div className="nav-logo">
-        <Link to="/">
-          <img width={100} src="FNH.png" />
-        </Link>
+      <div className="nav-header">
+        <div className="Logo">
+          <Link to="/">
+            <img width={100} src="FNH.png" />
+          </Link>
+        </div>
+
+        <div className="LogSet">
+          {authenticate ? (
+            <div onClick={() => setAuthenticate(false)}>
+              <FontAwesomeIcon icon={faUser} />
+              <span tyle={{ cursor: "pointer" }}>로그아웃</span>
+            </div>
+          ) : (
+            <div onClick={() => navigate("/login")}>
+              <FontAwesomeIcon icon={faUser} />
+              <span style={{ cursor: "pointer" }}>로그인</span>
+            </div>
+          )}
+        </div>
       </div>
+
       <div class="nav-menu-area">
         <ul className="menu">
           {menuList.map((menu, index) => (
