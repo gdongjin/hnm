8395e7fecf143f4671e751306aa2bd30e362dab7
GeunHyeok Jeong <jkh990521@gmail.com>
2023-09-04 17:00:46 +0900

    q

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,10 @@ import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import SplitButton from "react-bootstrap/SplitButton";
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
   const menuList = ["게시판", "정보제공", "고객센터", "마이페이지"];
@@ -58,10 +61,44 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
       <div class="nav-menu-area">
         <ul className="menu">
           {menuList.map((menu, index) => (
-            <li>
-              <a href="#" key={index}>
-                {menu}
-              </a>
+            <li key={index} className="Highdrop">
+              <Dropdown>
+                <Dropdown.Toggle variant="light" id={`dropdown-${menu}`}>
+                  {menu}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {menu === "게시판" && (
+                    <>
+                      <Dropdown.Item eventKey="1">
+                        커뮤니티 게시판
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="2">
+                        출조정보 게시판
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="3">
+                        조황정보 게시판
+                      </Dropdown.Item>
+                    </>
+                  )}
+                  {menu === "정보제공" && (
+                    <>
+                      <Dropdown.Item eventKey="1">어종정보</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">포인터 정보</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">날씨 정보</Dropdown.Item>
+                    </>
+                  )}
+                  {menu === "고객센터" && (
+                    <>
+                      <Dropdown.Item eventKey="1">공지사항</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">FAQ</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">Q&A</Dropdown.Item>
+                    </>
+                  )}
+                  {menu === "마이페이지" && (
+                    <Dropdown.Item eventKey="1">회원정보 수정</Dropdown.Item>
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
             </li>
           ))}
         </ul>
